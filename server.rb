b3a1be50c11321469c93410bb0be81e094d25ae6
9000 volts <htmlguy999@gmail.com>
2016-04-17 18:20:11 -0400

    Comment on the ruby code

--- server.rb
+++ server.rb
@@ -1,11 +1,19 @@
+# This ruby script is a server that can do really weird things.
 require 'sinatra'
+
+# Use the port that has (or hasn't) been specified as an arg.
 if !ARGV[0]
   abort 'YOU FOOL THERE\'S NO PORT SPECIFIED'
 end
 set :port, ARGV[0]
-get '/' do
+
+# Ok just send the homepage normally
+get '/?' do
   erb :main
 end
+
+# But if they want org data, run the orgs tool and send that data with the
+# homepage :D
 get '/orgs/?' do
   data = `python main.py orgs #{params['username']}`
   erb :main, :locals => {:data => data}
