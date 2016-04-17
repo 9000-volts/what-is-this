9dec42586c845e5f321c0091d92c60999c7ac2b9
9000 volts <htmlguy999@gmail.com>
2016-04-17 17:51:50 -0400

    COMMENTS

--- gh_orgs.py
+++ gh_orgs.py
@@ -1,11 +1,19 @@
-import requests
+# Okay so this file,
+# THIS FILE is gonna do some weird stuff ok?
+# we're gonna load up a fancy-shmancy list of a github user's orgs k?
+import requests # oh shit we need an external library oh no
 import json
 import sys
+# make sure they specified a user as the second program argument ok?
 if(len(sys.argv) < 3):
-    print('Specify a user!')
+    print('Specify a user!') # if they didn't, screw them! they can leave now
 else:
+    # ask github very kindly to give us some fancy information
     username = sys.argv[2]
     r = requests.get('https://api.github.com/users/%(username)s/orgs' % locals())
-    j = json.loads(r.text)
+    j = json.loads(r.text) # github speaks js so we ask our wonderful tour guide
+                           # jason to translate for us :D
+    # jason isn't too good with this language, let's clean up his stuff and
+    # present it all nice and fancy-like
     for i in j:
         print(i['login'] + " (" + i['description'] + ")")
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+# ok so here we go this program just has to launch some other programs in this
+# folder
 import sys
+# if a program hasn't been specified as the first argument, say so
 if(len(sys.argv) < 2):
     print('Choose a subprogram!')
+# oh ok good the user knows what they want
 else:
+    # oh cool this program can get github organization data
     if (sys.argv[1] == 'orgs'):
         import gh_orgs
+    # oh cool this program isn't a program, let's say so
     else:
         print('Invalid subprogram!')
