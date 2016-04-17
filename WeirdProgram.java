db1338abfcd484acd3d8d67050f97a18694d6688
9000 volts <htmlguy999@gmail.com>
2016-04-17 19:27:51 -0400

    GUI Java Program

--- WeirdProgram.java
+++ WeirdProgram.java
@@ -1,5 +1,17 @@
+import javax.swing.*;
 class WeirdProgram {
   public static void main(String[] args) {
-    System.out.println("Oh hey, I'm that weird program everybody is talking about.");
+    System.out.println("Oh hey, I'm that weird program everybody is talking about, and I'm starting a GUI.");
+
+    JFrame x = new JFrame("O Hai");
+    x.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    JPanel p = new JPanel();
+    x.add(p);
+
+    p.add(new JLabel("Hi! I am an instance of WeirdProgram, here to save the day!"));
+
+    x.pack();
+    x.setVisible(true);
   }
 }
