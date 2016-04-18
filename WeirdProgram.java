eafb277e22ce36a0e56e941b6ae50a9cc052489a
9000 volts <htmlguy999@gmail.com>
2016-04-17 20:47:31 -0400

    Weird comments

--- WeirdProgram.java
+++ WeirdProgram.java
@@ -1,25 +1,37 @@
 import javax.swing.*;
 import java.awt.event.*;
 import java.io.*;
+// Okay, this is a really complicated java program because everything in java
+// is way too complicated to accomplish your intended goal anyways.
 class WeirdProgram {
+  // In case you weren't aware already, this is the main method.
   public static void main(String[] args) {
-    System.out.println("Oh hey, I'm that weird program everybody is talking about, and I'm starting a GUI.");
 
+    // Ok here you go again, telling info about yourself to random strangers.
+    System.out.println("Oh hey, I'm that weird program everybody is talking about, and I'm starting a GUI.");
+    // You really gotta break that habit, WeirdProgram. seriously, it's dangerous
+
+    // Ok make a window and when it dies, it takes everybody down with it
     JFrame x = new JFrame("O Hai");
     x.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+    // Ok we need a panel to keep everything in all nice and snug
     JPanel p = new JPanel();
     x.add(p);
 
+    // Here is where we say random crap to the poor sap who decided to run xorg
     p.add(new JLabel("Hi! I am an instance of WeirdProgram, here to save the day!"));
     p.add(new JLabel("Don't you have anything better to do with your life?"));
 
+    // These weird buttons do weird stuff.
     JButton yes = new JButton("Yes");
     yes.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(ActionEvent e) {
+        // Tell them some popup and printout messages when they click.
         JOptionPane.showMessageDialog(x, "Go away and do it, please!");
         System.out.println("Lessee wat my perl friend has to say about this :D");
+        // Run our perl pal.
         // This block mostly copied from http://www.rgagnon.com/javadetails/java-0014.html
         try {
           String line;
@@ -42,10 +54,12 @@ class WeirdProgram {
         catch (Exception err) {
           err.printStackTrace();
         }
+        // Kill WeirdProgram. finally. i told you it was dangerous
         System.exit(0);
       }
     });
     JButton no = new JButton("No");
+    // ok when it's clicked, say some info just like yes and then die
     no.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(ActionEvent e) {
@@ -54,9 +68,12 @@ class WeirdProgram {
         System.exit(0);
       }
     });
+
+    // ok but the buttons need to be seen to be clicked please
     p.add(yes);
     p.add(no);
 
+    // squeeze everything in there nice and snug and make it show up.
     x.pack();
     x.setVisible(true);
   }
--- perlthing.pl
+++ perlthing.pl
@@ -1,4 +1,6 @@
 #!/usr/bin/perl
+# Ok so this perl program just says some stuff in order to be called by another
+# program. good.
 use strict;
 use warnings;
 print "Perl says: Somebody's making some effort in their life!\n"
